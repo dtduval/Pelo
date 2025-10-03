cdd8ba0fe279c6b9d0415e808e34eff7734260a7
Duval <dtduval@gmail.com>
2025-10-03 12:20:16 -0500

    Initial Commit

---
+++ Pelo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pelo
+//
+//  Created by David Timothy Duval on 2025-10-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pelo/PeloApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeloApp.swift
+//  Pelo
+//
+//  Created by David Timothy Duval on 2025-10-03.
+//
+
+import SwiftUI
+
+@main
+struct PeloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
